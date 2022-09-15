16b4f5bc162cec4ef97f0d04a6de77a0ad0fae1a
cr7yash <yashaqua@gmail.com>
2022-09-16 01:14:45 +0530

    handling background tasks

--- payment/main.py
+++ payment/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware 
+from fastapi.background import BackgroundTasks
 from redis_om import get_redis_connection,HashModel
 from starlette.requests import Request
-import requests
+import requests , time
 
 
 app = FastAPI()
@@ -34,9 +35,12 @@ class Order(HashModel):
     class Meta:
         database = redis
 
+@app.get('/orders/{pk}')
+def get(pk:str):
+    return Order.get(pk)
 
 @app.post('/orders')
-async def create(request: Request): #sending id and quantity
+async def create(request: Request, background_tasks:BackgroundTasks): #sending id and quantity
     body = await request.json()
 
     req = requests.get('http://localhost:8000/products/%s' % body['id'])
@@ -49,9 +53,16 @@ async def create(request: Request): #sending id and quantity
         fee = 0.2* product['price'],
         total = 1.2*product['price'],
         quantity = body['quantity'],
-        status = 'pending'
+        status = 'pending' 
     )
 
     order.save()
+
+    background_tasks.add_task(order_completed,order)
+
     return order
 
+def order_completed(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
